1e3f514701819329a4ed1a6b1deddcbde0b89a04
Willian Justen <willianjustenqui@gmail.com>
2017-04-25 12:19:12 +0200

    Returning Promise to test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 export const search = (query, type) =>
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(data => data.json())
 
 
 export const searchArtists = () => {}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -35,9 +35,11 @@ describe('Spotify Wrapper', () => {
 
   describe('Generic Search', () => {
     let fetchedStub;
+    let promise;
 
     beforeEach( () => {
       fetchedStub = sinon.stub(global, 'fetch');
+      promise = fetchedStub.returnsPromise();
     });
 
     afterEach( () => {
@@ -65,6 +67,13 @@ describe('Spotify Wrapper', () => {
         expect(fetchedStub).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
       });
+    });
+
+    it('should return the JSON Data from the Promise', () => {
+      promise.resolves({ body: 'json' });
+      const artists = search('Incubus', 'artist');
+
+      expect(artists.resolveValue).to.be.eql({ body: 'json' });
     });
   });
 });
