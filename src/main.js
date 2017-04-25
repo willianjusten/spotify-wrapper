470e2d56729859eeeee7f2631a6f269ff0942217
Willian Justen <willianjustenqui@gmail.com>
2017-04-25 16:19:19 +0200

    Fix lint styles

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
+/* global fetch */
+
 export const search = (query, type) =>
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then(data => data.json())
+    .then(data => data.json());
 
-export const searchArtists = (query) =>
+export const searchArtists = query =>
   search(query, 'artist');
 
-export const searchAlbums = (query) =>
+export const searchAlbums = query =>
   search(query, 'album');
 
-export const searchTracks = (query) =>
+export const searchTracks = query =>
   search(query, 'track');
 
-export const searchPlaylists = (query) =>
+export const searchPlaylists = query =>
   search(query, 'playlist');
