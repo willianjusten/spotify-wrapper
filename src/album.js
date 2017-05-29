e970d70b27991daa77c72676edee9defe9f6bf90
Willian Justen <willianjustenqui@gmail.com>
2017-05-29 13:28:33 -0300

    Add authorization flow

--- src/album.js
+++ src/album.js
@@ -1,13 +1,13 @@
 /* global fetch */
 
-import API_URL from './config';
+import { API_URL, HEADERS } from './config';
 import toJSON from './utils';
 
 export const getAlbum = id =>
-  fetch(`${API_URL}/albums/${id}`).then(toJSON);
+  fetch(`${API_URL}/albums/${id}`, HEADERS).then(toJSON);
 
 export const getAlbums = ids =>
-  fetch(`${API_URL}/albums/?ids=${ids}`).then(toJSON);
+  fetch(`${API_URL}/albums/?ids=${ids}`, HEADERS).then(toJSON);
 
 export const getAlbumTracks = id =>
-  fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON);
+  fetch(`${API_URL}/albums/${id}/tracks`, HEADERS).then(toJSON);
--- src/search.js
+++ src/search.js
@@ -1,9 +1,9 @@
 /* global fetch */
-import API_URL from './config';
+import { API_URL, HEADERS } from './config';
 import toJSON from './utils';
 
 export const search = (query, type) =>
-  fetch(`${API_URL}/search?q=${query}&type=${type}`)
+  fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS)
     .then(toJSON);
 
 export const searchArtists = query =>
