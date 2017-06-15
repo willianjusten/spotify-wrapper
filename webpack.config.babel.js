5930394b77c8a1d7484c657e18d1e3cca2ac3cf0
Willian Justen <willianjustenqui@gmail.com>
2017-06-15 18:52:35 -0300

    Edit to webpack UMD

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import {join} from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/index',
+  entry: './index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
