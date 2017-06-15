9882001c628e7fd37118ab4ce4abd9ed9dff3bfd
Willian Justen <willianjustenqui@gmail.com>
2017-06-15 17:07:51 -0300

    Creating the request method inside the Class

--- src/index.js
+++ src/index.js
@@ -12,10 +12,21 @@ import {
 } from './album';
 
 import { API_URL } from './config';
+import toJSON from './utils';
 
 export default class SpotifyWrapper {
   constructor(options) {
     this.apiURL = options.apiURL || API_URL;
     this.token = options.token;
+  }
+
+  request(url) {
+    const headers = {
+      headers: {
+        Authorization: `'Bearer ${this.token}'`,
+      },
+    };
+
+    return fetch(url, headers).then(toJSON);
   }
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,11 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+sinonStubPromise(sinon);
+chai.use(sinonChai);
+
+global.fetch = require('node-fetch');
 
 import SpotifyWrapper from '../src/index';
 
@@ -27,5 +34,58 @@ describe('SpotifyWrapper Library', function () {
     });
 
     expect(spotify.token).to.be.equal('foo');
+  });
+
+  describe('request method', () => {
+    let stubedFetch;
+    let promise;
+
+    beforeEach( () => {
+      stubedFetch = sinon.stub(global, 'fetch');
+      promise = stubedFetch.returnsPromise();
+    });
+
+    afterEach( () => {
+      stubedFetch.restore();
+    });
+
+    it('should have request method', () => {
+      let spotify = new SpotifyWrapper({});
+
+      expect(spotify.request).to.exist;
+    });
+
+    it('should call fetch when request', () => {
+      let spotify = new SpotifyWrapper({
+        token: 'foo'
+      });
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with right url passed', () => {
+      let spotify = new SpotifyWrapper({
+        token: 'foo'
+      });
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledWith('url');
+    });
+
+    it('should call fetch with right headers passed', () => {
+      let spotify = new SpotifyWrapper({
+        token: 'foo'
+      });
+
+      const headers = {
+        headers: {
+          Authorization: `'Bearer foo'`,
+        },
+      };
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledWith('url', headers);
+    });
   });
 });
