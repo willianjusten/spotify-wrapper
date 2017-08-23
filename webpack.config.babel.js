e020473e6be481c29d926d1e52c79a3fe7fa9fc8
Vitor Margis <vitor.margis@gmail.com>
2017-08-23 19:35:26 -0300

    Fix library on webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,7 +7,7 @@ export default {
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'spotifyWrapper',
+    library: 'SpotifyWrapper',
   },
   devtool: 'source-map',
   module: {
